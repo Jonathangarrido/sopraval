c2678f99db71f9f20e29c64b20364f937c5d3c8b
jonathan garrido <jonathan.garrido1988@gmail.com>
2018-03-06 20:16:41 -0300

    feat: receta imagen mas alta

--- src/js/index.build.js
+++ src/js/index.build.js
@@ -166,75 +166,6 @@ angular.module('lista', [])
     templateUrl: './js/component/common/lista/lista.html',
     controller: listaCtrl,
   });
-// **********************************************************
-// archivo component/common/menu
-// **********************************************************
-'use strict';
-
-menuCtrl.$inject = ['setDatos', '$location', '$scope', '$timeout'];
-function menuCtrl(setDatos, $location, $scope, $timeout) {
-  var vm = this;
-  vm.toggleFullscreen;
-  vm.toggleFullscreenActive;
-  vm.visible;
-
-  ////////////////////////////////////////////
-
-  vm.$onInit = function () {
-    vm.clickMenuFullscreen();
-    watchDatos();
-    menuActive();
-  };
-
-  vm.back = function () {
-
-    setDatos.setAnimate('fade');
-    $timeout(redireccionar, 100);
-    $timeout(menuActive, 100);
-    $timeout(function () {
-      vm.toggleFullscreen = false;
-      vm.toggleFullscreenActive = false;
-    }, 500);
-  };
-
-  function redireccionar() {
-    $location.path('/');
-  }
-
-  vm.menuFullscreen = function () {
-    vm.toggleFullscreen = !vm.toggleFullscreen;
-    vm.toggleFullscreenActive = vm.toggleFullscreen;
-  };
-
-  vm.clickMenuFullscreen = function () {
-    $('.menu-modal a').click(function () {
-      vm.toggleFullscreen = false;
-      vm.toggleFullscreenActive = false;
-    });
-  };
-
-  function watchDatos() {
-    $scope.$watch(function () {
-      return setDatos.visible;
-    }, function (newVal, oldVal) {
-      vm.visible = newVal;
-    });
-  }
-
-  function menuActive() {
-    $timeout(function () {
-      setDatos.setVisible(false);
-      $scope.$apply();
-    }, 100);
-  }
-
-}
-
-angular.module('menu', [])
-  .component('menu', {
-    templateUrl: './js/component/common/menu/menu.html',
-    controller: menuCtrl
-  });
 
 // **********************************************************
 // archivo component/pages/categoria
@@ -310,6 +241,75 @@ angular.module('categoria', [])
   .component('categoria', {
     templateUrl: './js/component/pages/categoria/categoria.html',
     controller: categoriaCtrl
+  });
+// **********************************************************
+// archivo component/common/menu
+// **********************************************************
+'use strict';
+
+menuCtrl.$inject = ['setDatos', '$location', '$scope', '$timeout'];
+function menuCtrl(setDatos, $location, $scope, $timeout) {
+  var vm = this;
+  vm.toggleFullscreen;
+  vm.toggleFullscreenActive;
+  vm.visible;
+
+  ////////////////////////////////////////////
+
+  vm.$onInit = function () {
+    vm.clickMenuFullscreen();
+    watchDatos();
+    menuActive();
+  };
+
+  vm.back = function () {
+
+    setDatos.setAnimate('fade');
+    $timeout(redireccionar, 100);
+    $timeout(menuActive, 100);
+    $timeout(function () {
+      vm.toggleFullscreen = false;
+      vm.toggleFullscreenActive = false;
+    }, 500);
+  };
+
+  function redireccionar() {
+    $location.path('/');
+  }
+
+  vm.menuFullscreen = function () {
+    vm.toggleFullscreen = !vm.toggleFullscreen;
+    vm.toggleFullscreenActive = vm.toggleFullscreen;
+  };
+
+  vm.clickMenuFullscreen = function () {
+    $('.menu-modal a').click(function () {
+      vm.toggleFullscreen = false;
+      vm.toggleFullscreenActive = false;
+    });
+  };
+
+  function watchDatos() {
+    $scope.$watch(function () {
+      return setDatos.visible;
+    }, function (newVal, oldVal) {
+      vm.visible = newVal;
+    });
+  }
+
+  function menuActive() {
+    $timeout(function () {
+      setDatos.setVisible(false);
+      $scope.$apply();
+    }, 100);
+  }
+
+}
+
+angular.module('menu', [])
+  .component('menu', {
+    templateUrl: './js/component/common/menu/menu.html',
+    controller: menuCtrl
   });
 // **********************************************************
 // archivo component/pages/home
