6360591667e4114f3527bfc8f71b5ac18de525d5
Jonathan Garrido <jonathan.garrido1988@gmail.com>
2018-03-12 08:47:29 -0300

    feat: RRSS y mail de contacto cyndy

--- src/php/formulario.php
+++ src/php/formulario.php
@@ -11,7 +11,7 @@
   $correo = $_POST["correo"];
   $comentario = $_POST["comentario"];
 
-  $destino = "jonathan.garrido1988@gmail.com";
+  $destino = "cyndy@agenciapan.cl";
 
   require("phpmailer/class.phpmailer.php");
   $mail = new PHPMailer();
